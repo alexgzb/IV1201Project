2ce3ac5dbe5a9a4b6ee87a389c8b1dc66da9d4e6
Wilhelm <>
2015-02-05 13:37:51 +0100

    Created a realm with db connection, AccountView, AccountController, started on a login page.

---
+++ src/main/java/se/kth/ict/iv1201/controller/AccountController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.kth.ict.iv1201.controller;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author iv1201
+ */
+@Stateless
+public class AccountController {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
